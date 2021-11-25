ab1b6ad93357259a56a8296df72c3de8a80750d6
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2021-11-25 14:47:51 -0500

    added ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Creates an object for the button element from the html.
+var buttonEl = document.querySelector("#save-task");
+// Creates an object for the tasks-to-do list element from the html.
+var tasksToDoEl = document.querySelector('#tasks-to-do');
+
+// Adds an event to the button. When clicked it calls on the createTaskHandler function which:
+//  - Defines listItemEl & creates a new 'li'
+//  - Gives listItemEl class name of 'task-item' for styling and text content of "This is a new task."
+//  - Appends listItemEl to tasksToDoEl. Applies the list item to the task to do list.
+buttonEl.addEventListener("click", createTaskHandler);
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
