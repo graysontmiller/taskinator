7053a29212060c7a3217c411ea3c848a96a97283
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2021-11-26 18:38:54 -0500

    Added functionality to creating tasks. Function now creates html dynamically to display input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,27 @@ var createTaskHandler = function() {
     //Prevents browser from refreshing when a submission is made
     event.preventDefault();
 
+    //Adds a querySelector to the Input of the html with the name 'task-name.'
+    //Dynamic way of making every input for that form into a querySelector.
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // This selects and stores just the value of the input.
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+
     tasksToDoEl.appendChild(listItemEl);
 }
 
