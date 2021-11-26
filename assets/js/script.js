bb1cd3ba52dbe256f1b436f2eaa6bf1ca45b5869
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2021-11-26 17:39:32 -0500

    Added submit event listener to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
-// Creates an object for the button element from the html.
-var buttonEl = document.querySelector("#save-task");
+// Creates an object for the form element from the html.
+var formEl = document.querySelector("#task-form");
 // Creates an object for the tasks-to-do list element from the html.
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 var createTaskHandler = function() {
+
+    //Prevents browser from refreshing when a submission is made
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-// Adds an event to the button. When clicked it calls on the createTaskHandler function which:
+// Adds an event to the form. When submit it calls on the createTaskHandler function which:
 //  - Defines listItemEl & creates a new 'li'
 //  - Gives listItemEl class name of 'task-item' for styling and text content of "This is a new task."
 //  - Appends listItemEl to tasksToDoEl. Applies the list item to the task to do list.
-buttonEl.addEventListener("click", createTaskHandler);
-// this means that on click, call back to previous function createTaskHandler.
+formEl.addEventListener("submit", createTaskHandler);
+// this means that on submit, call back to previous function createTaskHandler.
