abf770491bf69dee55b61c1db4e8cd95c77bf120
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2021-11-28 16:56:12 -0500

    refactored the code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var formEl = document.querySelector("#task-form");
 // Creates an object for the tasks-to-do list element from the html.
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
 
     //Prevents browser from refreshing when a submission is made
     event.preventDefault();
@@ -13,7 +13,19 @@ var createTaskHandler = function() {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     // This selects and stores just the value of the input.
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
+    // package the data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+// Create HTML based on the data for the task object (like name and type)
+var createTaskEl = function(taskDataObj) {
     // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -23,7 +35,7 @@ var createTaskHandler = function() {
     //give it a class name
     taskInfoEl.className = "task-info";
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
 
@@ -32,9 +44,9 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
 }
 
-// Adds an event to the form. When submit it calls on the createTaskHandler function which:
+// Adds an event to the form. When submit it calls on the taskFormHandler function which:
 //  - Defines listItemEl & creates a new 'li'
 //  - Gives listItemEl class name of 'task-item' for styling and text content of "This is a new task."
 //  - Appends listItemEl to tasksToDoEl. Applies the list item to the task to do list.
-formEl.addEventListener("submit", createTaskHandler);
-// this means that on submit, call back to previous function createTaskHandler.
+formEl.addEventListener("submit", taskFormHandler);
+// this means that on submit, call back to previous function taskFormHandler.
